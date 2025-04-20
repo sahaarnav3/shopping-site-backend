ce8b89037985ce9cf0164af6406b74f79fce5cd9
Arnav Saha <sahaarnav3@gmail.com>
2025-04-20 11:56:28 +0530

    Modified product schema to have an image link as well..

--- models/product.models.js
+++ models/product.models.js
@@ -33,6 +33,10 @@ const productSchema = new mongoose.Schema({
     type: [String],
     required: true
   },
+  imageLink: {
+    type: String,
+    required: true
+  },
   addedToWishlist: {
     type: Boolean,
     default: false
