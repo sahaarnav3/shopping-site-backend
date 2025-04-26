6e9094cc5b6bcb55bf1ca659505477ec3380d5aa
Arnav Saha <sahaarnav3@gmail.com>
2025-04-26 17:43:43 +0530

    Corrected a crucial mistake in product schema

--- models/product.models.js
+++ models/product.models.js
@@ -42,7 +42,7 @@ const productSchema = new mongoose.Schema({
     default: false
   },
   addedToCart: {
-    size: [String],
+    type: [String],
   },
   category: {
     type: mongoose.Schema.Types.ObjectId,
